5a291b18aabdfd10bc974e2e2eeebef668a5aaa2
Electronic89 <e_kharionovskiy@rambler.ru>
2019-06-08 11:58:48 +0300

    Делаем правки в коде

--- src/lesson2.java
+++ src/lesson2.java
@@ -1,4 +1,6 @@
-public class lesson2 {
+import Exception.*;
+
+public class lesson2  {
     public static void main(String[] args) {
         String[][] correct = {
                 {"1", "2", "3", "4"},
@@ -7,45 +9,79 @@ public class lesson2 {
                 {"1", "2", "3", "4"}
         };
         String[][] wrongSize = {
-                {"1", "2", "3", "4"},
+                {"1"},
                 {"1", "2"},
-                {"1", "2", "3", "4"},
+                {"1", "2", "3"},
                 {"1", "2", "3", "4"}
         };
         String[][] wrongChar = {
-                {"1", "2", "3", "4"},
                 {"1", "2", "a", "4"},
+                {"1", "2", "3", "4"},
                 {"1", "2", "3", "4"},
                 {"1", "2", "3", "4"}
         };
         try {
             try {
-                int result = method(correct);
+                int result = method(wrongChar);
                 System.out.println(result);
+
+
             } catch (MyArraySizeException e) {
-                System.out.println("Размер массива превышен!");
+
+                System.out.println(e.getError());
             }
         }
         catch (MyArrayDataException e) {
-            System.out.println("Неправильное значение массива!");
-            System.out.println("Ошибка в ячейке: " + e.i + "x" + e.j);
+            System.out.println(e.getStrError());
+        }
+        System.out.println("--------------------------");
+        try {
+            try {
+                int result = method(correct);
+                System.out.println(result);
+
+
+            } catch (MyArraySizeException e) {
+
+                System.out.println(e.getError());
+            }
+        }
+        catch (MyArrayDataException e) {
+            System.out.println(e.getStrError());
+        }
+        System.out.println("--------------------------");
+        try {
+            try {
+                int result = method(wrongSize);
+                System.out.println(result);
+
+
+            } catch (MyArraySizeException e) {
+
+                System.out.println(e.getError());
+            }
+        }
+        catch (MyArrayDataException e) {
+            System.out.println(e.getStrError());
         }
 
     }
 
 
-    public static int method(String[][] correct) throws MyArraySizeException, MyArrayDataException {
+
+
+    public static int method(String[][] array) throws MyArraySizeException, MyArrayDataException {
         int count = 0;
-        if (correct.length != 4) {
-            throw new MyArraySizeException();
+        if (array.length != 4) {
+            throw new MyArraySizeException("Размер массива превышен!");
         }
-        for (int i = 0; i < correct.length; i++) {
-            if (correct[i].length != 4) {
-                throw new MyArraySizeException();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException("Размер массива превышен!");
             }
-            for (int j = 0; j < correct[i].length; j++) {
+            for (int j = 0; j < array[i].length; j++) {
                 try {
-                    count = count + Integer.parseInt(correct[i][j]);
+                    count = count + Integer.parseInt(array[i][j]);
                 }
                 catch (NumberFormatException e) {
                     throw new MyArrayDataException(i, j);
